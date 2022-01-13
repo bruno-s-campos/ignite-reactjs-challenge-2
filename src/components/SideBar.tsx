5d79e1b9e92d18bc7d334e216103475d3243a4f3
Bruno Campos <bsc.ufam@gmail.com>
2022-01-13 01:48:36 -0300

    Add SideBar component
    Move the side bar logic to a new component called SideBar. Now it is
    responsible for rendering the movie categories at the left.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,58 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import '../styles/sidebar.scss';
+import { api } from '../services/api';
+
+import { Button } from './Button';
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+  setSelectedGenre: (genre: GenreResponseProps) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  const { selectedGenreId, setSelectedGenreId, setSelectedGenre } = props;
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  useEffect(() => {
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
